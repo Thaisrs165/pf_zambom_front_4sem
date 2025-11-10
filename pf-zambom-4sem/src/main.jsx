6983726ddba629007133d712a01d72185c4e2d7b
Thaisrs165 <thaisrs3@al.insper.edu.br>
2025-11-10 01:41:50 -0300

    auth0

--- pf-zambom-4sem/src/main.jsx
+++ pf-zambom-4sem/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+      domain="dev-ixloob8g7y8kjaf0.us.auth0.com"
+      clientId="sBLD574v15FhvNPJVxKpatYTvBAr6CKa"
+      authorizationParams={{
+        audience: "https://dev-ixloob8g7y8kjaf0.us.auth0.com/api/v2/",
+        redirect_uri: window.location.origin
+      }}
+    >
     <App />
-  </StrictMode>,
+  </Auth0Provider>, 
 )
